b94a2f3df3064e4b914a6c6c30f6f12d54196e09
keven01 <kevenchristiancode@gmail.com>
2026-09-05 01:14:01 -0300

    feat: EnemyAI seguindo o player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,9 +5,16 @@ public class EnemyAI : MonoBehaviour
 {
 
     private NavMeshAgent agent;
+    [SerializeField] GameObject playerObject;
     private Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = playerObject.transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
